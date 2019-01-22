e27fbfdd346d9e098dfab00a9ff2ab6c51f01bd2
Sinan Baltacioglu <siphon@gmail.com>
2019-01-21 23:09:00 -0500

    Time stamps and time format

--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 	fmt.Printf("hello, world\n")
+
+	t := time.Now()
+
+	fmt.Println(t.Format("2006-01-02 15:04:05"))
+	fmt.Println(t.UTC())
+	fmt.Println(t.Format(time.RFC850))
 }
